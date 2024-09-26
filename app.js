bb785815e43b46bfac87d2f964a91c721fc359c9
rani226 <120180821+rani226@users.noreply.github.com>
2024-09-26 14:18:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ class Chatbox {
           })
           .then(r => r.json())
           .then(r => {
-            let msg2 = { name: "Shera", message: r.answer };
+            let msg2 = { name: "Sara", message: r.answer };
             this.messages.push(msg2);
             this.updateChatText(chatbox)
             textField.value = ''
@@ -71,7 +71,7 @@ class Chatbox {
     updateChatText(chatbox) {
         var html = '';
         this.messages.slice().reverse().forEach(function(item, index) {
-            if (item.name === "Shera")
+            if (item.name === "Sara")
             {
                 html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
             }
